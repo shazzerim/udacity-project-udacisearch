9ffcc81e288b5436c1728ee05da6e991081f1784
Tina ZipZap <ti.zipzap@gmail.com>
2022-09-01 14:49:07 +0200

    wip parallel webcrawler

---
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/CrawlerTask.java
@@ -0,0 +1,152 @@
+package com.udacity.webcrawler;
+
+import com.udacity.webcrawler.parser.PageParser;
+import com.udacity.webcrawler.parser.PageParserFactory;
+
+import javax.inject.Inject;
+import java.time.Clock;
+import java.time.Instant;
+import java.util.*;
+import java.util.concurrent.RecursiveAction;
+import java.util.regex.Pattern;
+
+public final class CrawlerTask extends RecursiveAction {
+
+    private final Instant deadline;
+    private final int maxDepth;
+    private final Map<String, Integer> counts;
+    private final Set<String> visitedUrls;
+    private final String url;
+    private final List<Pattern> ignoredUrls;
+    private final Clock clock;
+
+    @Inject
+    PageParserFactory pageParserFactory;
+
+    private CrawlerTask(Instant deadline,
+                        int maxDepth,
+                        Map<String, Integer> counts,
+                        Set<String> visitedUrls,
+                        String url,
+                        List<Pattern> ignoredUrls,
+                        Clock clock) {
+
+        this.deadline = deadline;
+        this.maxDepth = maxDepth;
+        this.counts = counts;
+        this.visitedUrls = visitedUrls;
+        this.url = url;
+        this.ignoredUrls = ignoredUrls;
+        this.clock = clock;
+    }
+
+    public Instant getDeadline() {
+        return deadline;
+    }
+
+    public int getMaxDepth() {
+        return maxDepth;
+    }
+
+    public Map<String, Integer> getCounts() {
+        return counts;
+    }
+
+    public Set<String> getVisitedUrls() {
+        return visitedUrls;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public List<Pattern> getIgnoredUrls() {
+        return ignoredUrls;
+    }
+
+    @Override
+    protected void compute() {
+        if (maxDepth == 0 || clock.instant().isAfter(deadline)) {
+            return;
+        }
+        for (Pattern pattern : ignoredUrls) {
+            if (pattern.matcher(url).matches()) {
+                return;
+            }
+        }
+        if (visitedUrls.contains(url)) {
+            return;
+        }
+        visitedUrls.add(url);
+        PageParser.Result result = pageParserFactory.get(url).parse();
+        for (Map.Entry<String, Integer> e : result.getWordCounts().entrySet()) {
+            if (counts.containsKey(e.getKey())) {
+                counts.put(e.getKey(), e.getValue() + counts.get(e.getKey()));
+            } else {
+                counts.put(e.getKey(), e.getValue());
+            }
+        }
+
+        List<CrawlerTask> subtasks = new ArrayList<>();
+
+        result.getLinks().forEach(subUrl -> subtasks.add(new CrawlerTask(
+                deadline,
+                maxDepth - 1,
+                counts,
+                visitedUrls,
+                subUrl,
+                ignoredUrls,
+                clock
+        )));
+
+        invokeAll(subtasks);
+
+    }
+
+    public static final class CrawlerTaskBuilder {
+
+        private Instant deadline;
+        @MaxDepth
+        private int maxDepth;
+        private Map<String, Integer> counts;
+        private Set<String> visitedUrls;
+        @IgnoredUrls
+        private List<Pattern> ignoredUrls;
+
+        private String url;
+        private Clock clock;
+
+        public CrawlerTaskBuilder setDeadline(Instant deadline) {
+            this.deadline = deadline;
+            return this;
+        }
+
+        public CrawlerTaskBuilder setCounts(Map<String, Integer> counts) {
+            this.counts = counts;
+            return this;
+        }
+
+        public CrawlerTaskBuilder setVisitedUrls(Set<String> visitedUrls) {
+            this.visitedUrls = visitedUrls;
+            return this;
+        }
+
+        public CrawlerTaskBuilder setUrl(String url) {
+            this.url = url;
+            return this;
+        }
+
+        public CrawlerTaskBuilder setClock(Clock clock) {
+            this.clock = clock;
+            return this;
+        }
+
+        public CrawlerTask build() {
+            return new CrawlerTask(deadline, maxDepth, counts, visitedUrls, url, ignoredUrls, clock);
+        }
+
+
+    }
+
+
+}
--- starter/webcrawler/src/main/java/com/udacity/webcrawler/ParallelWebCrawler.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/ParallelWebCrawler.java
@@ -3,47 +3,69 @@ package com.udacity.webcrawler;
 import com.udacity.webcrawler.json.CrawlResult;
 
 import javax.inject.Inject;
-import javax.inject.Provider;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.*;
 import java.util.concurrent.ForkJoinPool;
-import java.util.stream.Collectors;
 
 /**
  * A concrete implementation of {@link WebCrawler} that runs multiple threads on a
  * {@link ForkJoinPool} to fetch and process multiple web pages in parallel.
  */
 final class ParallelWebCrawler implements WebCrawler {
-  private final Clock clock;
-  private final Duration timeout;
-  private final int popularWordCount;
-  private final ForkJoinPool pool;
+    private final Clock clock;
+    private final Duration timeout;
+    private final int popularWordCount;
+    private final ForkJoinPool pool;
 
-  @Inject
-  ParallelWebCrawler(
-      Clock clock,
-      @Timeout Duration timeout,
-      @PopularWordCount int popularWordCount,
-      @TargetParallelism int threadCount) {
-    this.clock = clock;
-    this.timeout = timeout;
-    this.popularWordCount = popularWordCount;
-    this.pool = new ForkJoinPool(Math.min(threadCount, getMaxParallelism()));
-  }
+    @Inject
+    ParallelWebCrawler(
+            Clock clock,
+            @Timeout Duration timeout,
+            @PopularWordCount int popularWordCount,
+            @TargetParallelism int threadCount) {
+        this.clock = clock;
+        this.timeout = timeout;
+        this.popularWordCount = popularWordCount;
+        this.pool = new ForkJoinPool(Math.min(threadCount, getMaxParallelism()));
+    }
 
-  @Override
-  public CrawlResult crawl(List<String> startingUrls) {
-    return new CrawlResult.Builder().build();
-  }
+    @Override
+    public CrawlResult crawl(List<String> startingUrls) {
 
-  @Override
-  public int getMaxParallelism() {
-    return Runtime.getRuntime().availableProcessors();
-  }
+        Instant deadline = clock.instant().plus(timeout);
+        Map<String, Integer> counts = Collections.synchronizedMap(new HashMap<>());
+        Set<String> visitedUrls = Collections.synchronizedSet(new HashSet<>());
+
+        CrawlerTask.CrawlerTaskBuilder crawlerTaskBuilder = new CrawlerTask
+                .CrawlerTaskBuilder()
+                .setCounts(counts)
+                .setDeadline(deadline)
+                .setVisitedUrls(visitedUrls)
+                .setClock (clock);
+
+       for (String url : startingUrls) {
+            pool.invoke(crawlerTaskBuilder.setUrl(url).build());
+        }
+
+        if (counts.isEmpty()) {
+            return new CrawlResult.Builder()
+                    .setWordCounts(counts)
+                    .setUrlsVisited(visitedUrls.size())
+                    .build();
+        }
+
+        return new CrawlResult.Builder()
+                .setWordCounts(WordCounts.sort(counts, popularWordCount))
+                .setUrlsVisited(visitedUrls.size())
+                .build();
+
+    }
+
+
+    @Override
+    public int getMaxParallelism() {
+        return Runtime.getRuntime().availableProcessors();
+    }
 }
